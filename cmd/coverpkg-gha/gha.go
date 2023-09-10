3a1e642bedc198c499c5325cab44b2000eee87b1
Michael Urman <MichaelUrman@users.noreply.github.com>
2023-09-09 22:50:19 -0500

    Add Notice annotations, line/col ends, titles

--- cmd/coverpkg-gha/gha.go
+++ cmd/coverpkg-gha/gha.go
@@ -21,76 +21,78 @@ var ghaEscaper = strings.NewReplacer("%", "%25", "\n", "%0A", "\r", "%0D")
 // Use like gha.At(filename [, line, col]).Error(information...)
 // File is required (or skip use of At); line and col are optional.
 func (gha *GitHubAction) At(file string, linecol ...int) *ghaPos {
-	if len(linecol) > 1 {
-		return &ghaPos{gha.w, file, linecol[0], linecol[1]}
-	} else if len(linecol) > 0 {
-		return &ghaPos{gha.w, file, linecol[0], 0}
+	pos := &ghaPos{w: gha.w, file: file}
+	if len(linecol) > 0 {
+		pos.line = linecol[0]
 	}
-	return &ghaPos{w: gha.w, file: file}
+	if len(linecol) > 1 {
+		pos.col = linecol[1]
+	}
+	return pos
 }
 
 // Debug emits a debug message. GitHub only shows these if you've set secret:
 //
 //	ACTIONS_STEP_DEBUG=true
-func (gha *GitHubAction) Debug(a ...interface{}) {
+func (gha *GitHubAction) Debug(a ...any) {
 	fmt.Fprintf(gha.w, "::debug::%s\n", sprint(a))
 }
 
 // Debugf emits a debug message. GitHub only shows these if you've set secret:
 //
 //	ACTIONS_STEP_DEBUG=true
-func (gha *GitHubAction) Debugf(format string, a ...interface{}) {
+func (gha *GitHubAction) Debugf(format string, a ...any) {
 	fmt.Fprintf(gha.w, "::debug::%s\n", sprintf(format, a))
 }
 
 // Print emits regular output.
-func (gha *GitHubAction) Print(a ...interface{}) {
+func (gha *GitHubAction) Print(a ...any) {
 	fmt.Fprintf(gha.w, "%s\n", sprint(a))
 }
 
 // Printf emits regular output.
-func (gha *GitHubAction) Printf(format string, a ...interface{}) {
+func (gha *GitHubAction) Printf(format string, a ...any) {
 	fmt.Fprintf(gha.w, "%s\n", sprintf(format, a))
 }
 
 // Error emits an error message.
-func (gha *GitHubAction) Error(a ...interface{}) {
+func (gha *GitHubAction) Error(a ...any) {
 	fmt.Fprintf(gha.w, "::error::%s\n", sprint(a))
 }
 
 // Errorf emits an error message.
-func (gha *GitHubAction) Errorf(format string, a ...interface{}) {
+func (gha *GitHubAction) Errorf(format string, a ...any) {
 	fmt.Fprintf(gha.w, "::error::%s\n", sprintf(format, a))
 }
 
 // ErrorAt emits an error message at a position. Set line and column to 0 to omit.
-func (gha *GitHubAction) ErrorAt(file string, line, col int, a ...interface{}) {
-	(&ghaPos{gha.w, file, line, col}).Error(a...)
+func (gha *GitHubAction) ErrorAt(file string, line, col int, a ...any) {
+	gha.At(file, line, col).Error(a...)
 }
 
 // ErrorAtf emits an error message at a position. Set line and column to 0 to omit.
-func (gha *GitHubAction) ErrorAtf(file string, line, col int, format string, a ...interface{}) {
-	(&ghaPos{gha.w, file, line, col}).Errorf(format, a...)
+func (gha *GitHubAction) ErrorAtf(file string, line, col int, format string, a ...any) {
+	gha.At(file, line, col).Errorf(format, a...)
 }
 
 // Warning emits an warning message.
-func (gha *GitHubAction) Warning(a ...interface{}) {
+func (gha *GitHubAction) Warning(a ...any) {
 	fmt.Fprintf(gha.w, "::warning::%s\n", sprint(a))
 }
 
 // Warningf emits an warning message.
-func (gha *GitHubAction) Warningf(format string, a ...interface{}) {
+func (gha *GitHubAction) Warningf(format string, a ...any) {
 	fmt.Fprintf(gha.w, "::warning::%s\n", sprintf(format, a))
 }
 
 // WarningAt emits an warning message at a position. Set line and column to 0 to omit.
-func (gha *GitHubAction) WarningAt(file string, line, col int, a ...interface{}) {
-	(&ghaPos{gha.w, file, line, col}).Warning(a...)
+func (gha *GitHubAction) WarningAt(file string, line, col int, a ...any) {
+	gha.At(file, line, col).Warning(a...)
 }
 
 // WarningAtf emits an warning message at a position. Set line and column to 0 to omit.
-func (gha *GitHubAction) WarningAtf(file string, line, col int, format string, a ...interface{}) {
-	(&ghaPos{gha.w, file, line, col}).Warningf(format, a...)
+func (gha *GitHubAction) WarningAtf(file string, line, col int, format string, a ...any) {
+	gha.At(file, line, col).Warningf(format, a...)
 }
 
 // Group groups output in a GitHub actions log
@@ -165,29 +167,46 @@ func (gha *GitHubAction) Event(path string) *GitHubEvent {
 
 // ghaPos reports a file location for Error and Warning messages.
 type ghaPos struct {
-	w    io.Writer
-	file string
-	line int
-	col  int
+	w             io.Writer
+	title         string
+	file          string
+	line, lineEnd int
+	col, colEnd   int
+}
+
+func (pos *ghaPos) Title(title string) *ghaPos   { pos.title = title; return pos }
+func (pos *ghaPos) Line(start int) *ghaPos       { return pos.Lines(start, 0) }
+func (pos *ghaPos) Lines(start, end int) *ghaPos { pos.line, pos.lineEnd = start, end; return pos }
+func (pos *ghaPos) Col(start int) *ghaPos        { return pos.Cols(start, 0) }
+func (pos *ghaPos) Cols(start, end int) *ghaPos  { pos.col, pos.colEnd = start, end; return pos }
+
+// Notice emits an annotation.
+func (pos *ghaPos) Notice(a ...any) {
+	fmt.Fprintf(pos.w, "::notice%v::%s\n", pos, sprint(a))
+}
+
+// Noticef emits an annotation.
+func (pos *ghaPos) Noticef(format string, a ...any) {
+	fmt.Fprintf(pos.w, "::notice%v::%s\n", pos, sprintf(format, a))
 }
 
 // Error emits an error message.
-func (pos *ghaPos) Error(a ...interface{}) {
+func (pos *ghaPos) Error(a ...any) {
 	fmt.Fprintf(pos.w, "::error%v::%s\n", pos, sprint(a))
 }
 
 // Errorf emits an error message.
-func (pos *ghaPos) Errorf(format string, a ...interface{}) {
+func (pos *ghaPos) Errorf(format string, a ...any) {
 	fmt.Fprintf(pos.w, "::error%v::%s\n", pos, sprintf(format, a))
 }
 
 // Warning emits an warning message.
-func (pos *ghaPos) Warning(a ...interface{}) {
+func (pos *ghaPos) Warning(a ...any) {
 	fmt.Fprintf(pos.w, "::warning%v::%s\n", pos, sprint(a))
 }
 
 // Warningf emits an warning message.
-func (pos *ghaPos) Warningf(format string, a ...interface{}) {
+func (pos *ghaPos) Warningf(format string, a ...any) {
 	fmt.Fprintf(pos.w, "::warning%v::%s\n", pos, sprintf(format, a))
 }
 
@@ -196,24 +215,33 @@ func (p *ghaPos) Format(f fmt.State, r rune) {
 		return
 	}
 	fmt.Fprintf(f, " file=%s", p.file)
+	if p.title != "" {
+		fmt.Fprintf(f, ",title=%s", ghaUnsafe(p.title))
+	}
 	if p.line == 0 {
 		return
 	}
 	fmt.Fprintf(f, ",line=%d", p.line)
+	if p.lineEnd != 0 {
+		fmt.Fprintf(f, ",endLine=%d", p.lineEnd)
+	}
 	if p.col == 0 {
 		return
 	}
 	fmt.Fprintf(f, ",col=%d", p.col)
+	if p.colEnd != 0 {
+		fmt.Fprintf(f, ",endColumn=%d", p.colEnd)
+	}
 }
 
 type ghaUnsafe string
 
-func sprint(a []interface{}) ghaUnsafe {
+func sprint(a []any) ghaUnsafe {
 	s := fmt.Sprintln(a...)
 	return ghaUnsafe(s[:len(s)-1])
 }
 
-func sprintf(f string, a []interface{}) ghaUnsafe {
+func sprintf(f string, a []any) ghaUnsafe {
 	return ghaUnsafe(fmt.Sprintf(f, a...))
 }
 
@@ -229,7 +257,7 @@ func (s ghaUnsafe) Format(f fmt.State, r rune) {
 	}
 }
 
-type GitHubEvent map[string]interface{}
+type GitHubEvent map[string]any
 
 func (ghe GitHubEvent) String(log diag.Interface, path string) string {
 	v := ghe.lookup(log, path)
@@ -249,11 +277,11 @@ func (ghe GitHubEvent) Int(log diag.Interface, path string) int {
 	return 0
 }
 
-func (ghe GitHubEvent) lookup(log diag.Interface, path string) interface{} {
+func (ghe GitHubEvent) lookup(log diag.Interface, path string) any {
 	paths := strings.Split(path, ".")
-	var src interface{} = (map[string]interface{})(ghe)
+	var src any = (map[string]any)(ghe)
 	for _, p := range paths {
-		if v, ok := src.(map[string]interface{}); ok {
+		if v, ok := src.(map[string]any); ok {
 			keys := make([]string, 0, len(v))
 			for k := range v {
 				keys = append(keys, k)
@@ -265,7 +293,7 @@ func (ghe GitHubEvent) lookup(log diag.Interface, path string) interface{} {
 			}
 			diag.Warningf(log, "invalid event path %q (%s) in %T: %v", p, path, src, keys)
 			return nil
-		} else if v, ok := src.([]interface{}); ok {
+		} else if v, ok := src.([]any); ok {
 			i, err := strconv.Atoi(p)
 			if err != nil || i >= len(v) || i < 0 {
 				diag.Warningf(log, "invalid event path %q (%s) in %T: 0..%d", p, path, src, len(v))
--- cmd/coverpkg-gha/gha_test.go
+++ cmd/coverpkg-gha/gha_test.go
@@ -15,198 +15,202 @@ func TestFullDiagInterface(t *testing.T) {
 	}
 }
 
+func TestAt(t *testing.T) {
+	w := &output{}
+	gha := &GitHubAction{w}
+
+	at := gha.At("file.go")
+	at.Notice("file-notice")
+	w.Want(t, "::notice file=file.go::file-notice\n")
+	at.Noticef("%q", "a")
+	w.Want(t, "::notice file=file.go::\"a\"\n")
+
+	at.Line(10).Notice("line-notice")
+	w.Want(t, "::notice file=file.go,line=10::line-notice\n")
+	at.Lines(2, 4).Notice("linerange-notice")
+	w.Want(t, "::notice file=file.go,line=2,endLine=4::linerange-notice\n")
+
+	at.Line(20).Col(6).Notice("col-notice")
+	w.Want(t, "::notice file=file.go,line=20,col=6::col-notice\n")
+	at.Cols(6, 12).Notice("colrange-notice")
+	w.Want(t, "::notice file=file.go,line=20,col=6,endColumn=12::colrange-notice\n")
+
+	at.Line(0).Title("nopos").Notice("titled-notice")
+	w.Want(t, "::notice file=file.go,title=nopos::titled-notice\n")
+}
+
 func TestDebug(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.Debug("a", "b", "c")
-	wantOutput(t, "stdout", w.String(), "::debug::a b c\n")
+	w.Want(t, "::debug::a b c\n")
 
-	w.Reset()
 	gha.Debugf("%q", "a")
-	wantOutput(t, "stdout", w.String(), "::debug::\"a\"\n")
+	w.Want(t, "::debug::\"a\"\n")
 
-	w.Reset()
 	gha.Debug("a\nb")
-	wantOutput(t, "stdout", w.String(), "::debug::a%0Ab\n")
+	w.Want(t, "::debug::a%0Ab\n")
 
-	w.Reset()
 	gha.Debug("a%b")
-	wantOutput(t, "stdout", w.String(), "::debug::a%25b\n")
+	w.Want(t, "::debug::a%25b\n")
 }
 
 func TestPrint(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.Print("a", "b", "c")
-	wantOutput(t, "stdout", w.String(), "a b c\n")
+	w.Want(t, "a b c\n")
 
-	w.Reset()
 	gha.Printf("%q", "a")
-	wantOutput(t, "stdout", w.String(), "\"a\"\n")
+	w.Want(t, "\"a\"\n")
 
-	w.Reset()
 	gha.Print("a\nb")
-	wantOutput(t, "stdout", w.String(), "a%0Ab\n")
+	w.Want(t, "a%0Ab\n")
 
-	w.Reset()
 	gha.Print("a%b")
-	wantOutput(t, "stdout", w.String(), "a%25b\n")
+	w.Want(t, "a%25b\n")
 }
 
 func TestError(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.Error("a", "b", "c")
-	wantOutput(t, "stdout", w.String(), "::error::a b c\n")
+	w.Want(t, "::error::a b c\n")
 
-	w.Reset()
 	gha.Errorf("%q", "a")
-	wantOutput(t, "stdout", w.String(), "::error::\"a\"\n")
+	w.Want(t, "::error::\"a\"\n")
 
-	w.Reset()
 	gha.Error("a\nb")
-	wantOutput(t, "stdout", w.String(), "::error::a%0Ab\n")
+	w.Want(t, "::error::a%0Ab\n")
 
-	w.Reset()
 	gha.Error("a%b")
-	wantOutput(t, "stdout", w.String(), "::error::a%25b\n")
+	w.Want(t, "::error::a%25b\n")
 
-	w.Reset()
 	gha.ErrorAt("f.go", 23, 42, "a")
-	wantOutput(t, "stdout", w.String(), "::error file=f.go,line=23,col=42::a\n")
+	w.Want(t, "::error file=f.go,line=23,col=42::a\n")
 
-	w.Reset()
 	gha.ErrorAt("f.go", 23, 0, "a")
-	wantOutput(t, "stdout", w.String(), "::error file=f.go,line=23::a\n")
+	w.Want(t, "::error file=f.go,line=23::a\n")
 
-	w.Reset()
 	gha.ErrorAt("f.go", 0, 0, "a")
-	wantOutput(t, "stdout", w.String(), "::error file=f.go::a\n")
+	w.Want(t, "::error file=f.go::a\n")
 
-	w.Reset()
 	gha.ErrorAtf("f.go", 0, 0, "%q", "a")
-	wantOutput(t, "stdout", w.String(), "::error file=f.go::\"a\"\n")
+	w.Want(t, "::error file=f.go::\"a\"\n")
 }
 
 func TestWarning(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.Warning("a", "b", "c")
-	wantOutput(t, "stdout", w.String(), "::warning::a b c\n")
+	w.Want(t, "::warning::a b c\n")
 
-	w.Reset()
 	gha.Warningf("%q", "a")
-	wantOutput(t, "stdout", w.String(), "::warning::\"a\"\n")
+	w.Want(t, "::warning::\"a\"\n")
 
-	w.Reset()
 	gha.Warning("a\nb")
-	wantOutput(t, "stdout", w.String(), "::warning::a%0Ab\n")
+	w.Want(t, "::warning::a%0Ab\n")
 
-	w.Reset()
 	gha.Warning("a%b")
-	wantOutput(t, "stdout", w.String(), "::warning::a%25b\n")
+	w.Want(t, "::warning::a%25b\n")
 
-	w.Reset()
 	gha.WarningAt("f.go", 23, 42, "a")
-	wantOutput(t, "stdout", w.String(), "::warning file=f.go,line=23,col=42::a\n")
+	w.Want(t, "::warning file=f.go,line=23,col=42::a\n")
 
-	w.Reset()
 	gha.WarningAt("f.go", 23, 0, "a")
-	wantOutput(t, "stdout", w.String(), "::warning file=f.go,line=23::a\n")
+	w.Want(t, "::warning file=f.go,line=23::a\n")
 
-	w.Reset()
 	gha.WarningAt("f.go", 0, 0, "a")
-	wantOutput(t, "stdout", w.String(), "::warning file=f.go::a\n")
+	w.Want(t, "::warning file=f.go::a\n")
 
-	w.Reset()
 	gha.WarningAtf("f.go", 0, 0, "%q", "a")
-	wantOutput(t, "stdout", w.String(), "::warning file=f.go::\"a\"\n")
+	w.Want(t, "::warning file=f.go::\"a\"\n")
 }
 
 func TestGroup(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 
 	gha.Group("some%group", func(i diag.Interface) {
 		i.Print("printed")
 	})
-	wantOutput(t, "stdout", w.String(), "::group::some%25group\nprinted\n::endgroup::\n")
+	w.Want(t, "::group::some%25group\nprinted\n::endgroup::\n")
 }
 
 func TestMask(t *testing.T) {
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 
 	gha.MaskValue("secret%value")
-	wantOutput(t, "stdout", w.String(), "::add-mask::secret%25value\n")
+	w.Want(t, "::add-mask::secret%25value\n")
 }
 
 func TestSetEnv(t *testing.T) {
 	withTempName(t, func(envs string) {
 		withCfg(func() {
 			cfg.SetEnv = envs
-			w := &strings.Builder{}
+			w := &output{}
 			gha := &GitHubAction{w}
 			gha.SetEnv("name", "value")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, envs, "name=value\n")
 
 			gha.SetEnv("name2", "value\n2")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, envs, "name=value\nname2=<<END_name2\nvalue\n2\nEND_name2\n")
 		})
 	})
 
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.SetEnv("name", "value")
-	wantOutput(t, "stdout", w.String(), "::error::GITHUB_ENV not available\n")
+	w.Want(t, "::error::GITHUB_ENV not available\n")
 }
 
 func TestSetOutput(t *testing.T) {
 	withTempName(t, func(outs string) {
 		withCfg(func() {
 			cfg.SetOutput = outs
-			w := &strings.Builder{}
+			w := &output{}
 			gha := &GitHubAction{w}
 
 			gha.SetOutput("name", "value")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, outs, "name=value\n")
 
 			gha.SetOutput("name2", "value2")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, outs, "name=value\nname2=value2\n")
 		})
 	})
 
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.SetOutput("name", "value")
-	wantOutput(t, "stdout", w.String(), "::error::GITHUB_OUTPUT not available\n")
+	w.Want(t, "::error::GITHUB_OUTPUT not available\n")
 }
 
 func TestSetPath(t *testing.T) {
 	withTempName(t, func(paths string) {
 		withCfg(func() {
 			cfg.SetPath = paths
-			w := &strings.Builder{}
+			w := &output{}
 			gha := &GitHubAction{w}
 
 			gha.AddPath("/some/dir")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, paths, "/some/dir\n")
 
 			gha.AddPath("/another/dir")
-			wantOutput(t, "stdout", w.String(), "")
+			w.Want(t, "")
 			wantFileContent(t, paths, "/some/dir\n/another/dir\n")
 		})
 	})
 
-	w := &strings.Builder{}
+	w := &output{}
 	gha := &GitHubAction{w}
 	gha.AddPath("/some/dir")
-	wantOutput(t, "stdout", w.String(), "::error::GITHUB_PATH not available\n")
+	w.Want(t, "::error::GITHUB_PATH not available\n")
 }
 
 func withCfg(fn func()) {
@@ -236,8 +240,14 @@ func wantFileContent(t *testing.T, name, content string) {
 	}
 }
 
-func wantOutput(t *testing.T, label, got, want string) {
-	if got != want {
-		t.Errorf("%s: got %q, want %q", label, got, want)
+type output struct {
+	strings.Builder
+}
+
+func (o *output) Want(t *testing.T, want string) {
+	t.Helper()
+	if got := o.String(); got != want {
+		t.Errorf("stdout: got %q, want %q", got, want)
 	}
+	o.Reset()
 }
